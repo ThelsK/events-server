bd2dad8964b5972eecfd6ff336f4ae64f4b0ba04
ThelsK <github@thels.nl>
2019-09-05 14:50:50 +0200

    Setup event routers

--- event/router.js
+++ event/router.js
@@ -1,3 +1,47 @@
 const Event = require("./model")
 const eventRouter = new require("express").Router()
+
+eventRouter.get("/event", (req, res, next) => {
+  Event.findAll()
+    .then(events => res.send(events))
+    .catch(next)
+})
+
+eventRouter.get("/event/:id", (req, res, next) => {
+  Event.findByPk(req.params.id)
+    .then(event => {
+      if (event) {
+        res.send(event)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(next)
+})
+
+eventRouter.post("/event", (req, res, next) => {
+  Event.create(req.body)
+    .then(event => res.send(event))
+    .catch(next)
+})
+
+eventRouter.put("/event/:id", (req, res, next) => {
+  Event.findByPk(req.params.id)
+    .then(event => {
+      if (event) {
+        event.update(req.body)
+          .then(event => res.send(event))
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(next)
+})
+
+eventRouter.delete("/event/:id", (req, res, next) => {
+  Event.destroy({ where: { id: req.params.id } })
+    .then(amount => res.send(amount))
+    .catch(next)
+})
+
 module.exports = eventRouter
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require("express")()
 const port = process.env.PORT || 4000
 
-app.use(require("cors")())
-app.use(require("body-parser").json())
-
-app.use(require("./event/router"))
+app.use(
+  require("cors")(),
+  require("body-parser").json(),
+  require("./event/router"),
+)
 
 app.listen(port, () => console.log(`Listening on :${port}`))
